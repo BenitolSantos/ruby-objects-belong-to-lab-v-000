39971864c500829f6d7dada7b8caa9f3edc4381f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-04 23:04:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,6 @@ class Artist
   attr_accessor :name, :genre #no need for initialization check the spec.
   #its like a baby. the name isn't given while it's in the womb.
   #but when it is born.
-  #when the method is called... then the connects are made between
+  #when the method is called... then the connection are made between
 
 end
