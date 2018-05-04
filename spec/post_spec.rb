480b84922ce0df51ebb303c6c1fe45586cd7a367
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-04 23:06:06 +0000

    automatically backed up by learn

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,19 +1,20 @@
 require "spec_helper"
 
 describe "Post" do
-  let!(:post) { Post.new } 
+  let!(:post) { Post.new }
   it "has a title" do
-    post.title = "Hello World" 
+    post.title = "Hello World"
     expect(post.title).to eq("Hello World")
   end
 
-  it "belongs to an author" do 
+  it "belongs to an author" do
     author = Author.new #setting a new instance of Author class
-    post.author = author #setting the instance equal to the author attribute
+    post.author = author #setting the instance equal to the author attribute in
+    #the post class
     expect(post.author).to eq(author)
   end
 
-  it "knows the name of the author it belongs to" do 
+  it "knows the name of the author it belongs to" do
     author = Author.new
     author.name = "Uncle Bob"
     post.author = author
